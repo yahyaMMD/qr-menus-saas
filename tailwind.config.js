428d0752aad193b02df1912542b2085e1be3eb04
Yassir CHERDOUH <yassir.cherdouh13@gmail.com>
2025-11-06 05:02:18 +0100

    fix: Fix dependencies issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content: [],
+    content: [
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+    	"./components/**/*.{js,ts,jsx,tsx,mdx}",
+	],
   theme: {
   	extend: {
   		borderRadius: {
